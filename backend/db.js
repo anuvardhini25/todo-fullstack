567bafbc2a1bb70e8172238dd6334786d3a6424e
Anuvardhini <23203006@rmd.ac.in>
2026-05-25 17:38:16 +0530

    Added screenshots

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "jesus@2005",
+  password:"your password",
   database: "todo_app"
 });
 
